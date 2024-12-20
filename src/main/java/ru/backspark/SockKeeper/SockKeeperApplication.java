f0b146944f641109769537d394ba0e927b446340
lin-_da@mail.ru <lin-_da@mail.ru>
2024-12-20 16:43:38 +0300

    Initial commit

---
+++ src/main/java/ru/backspark/SockKeeper/SockKeeperApplication.java
@@ -0,0 +1,13 @@
+package ru.backspark.SockKeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SockKeeperApplication.class, args);
+	}
+
+}
